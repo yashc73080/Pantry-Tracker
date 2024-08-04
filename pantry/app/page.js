d8dfa295730bec01c40c99ee9c0860a5557738dc
Yash Chennawar <144838733+yashc73080@users.noreply.github.com>
2024-08-04 14:25:53 -0400

    made postbuild script

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,21 +1,27 @@
-'use client';
+// // 'use client';
 
-import { ErrorBoundary } from 'react-error-boundary';
+// import { ErrorBoundary } from 'react-error-boundary';
+// import PantryClient from './PantryClient';
+
+// function ErrorFallback({error}) {
+//   return (
+//     <div role="alert">
+//       <p>Something went wrong:</p>
+//       <pre>{error.message}</pre>
+//     </div>
+//   )
+// }
+
+// export default function Home() {
+//   return (
+//     <ErrorBoundary FallbackComponent={ErrorFallback}>
+//       <PantryClient />
+//     </ErrorBoundary>
+//   );
+// }
+
 import PantryClient from './PantryClient';
 
-function ErrorFallback({error}) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-    </div>
-  )
-}
-
 export default function Home() {
-  return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <PantryClient />
-    </ErrorBoundary>
-  );
+  return <PantryClient />;
 }
