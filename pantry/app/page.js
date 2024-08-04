8396c874da34f04f5e9509ce58e6bf2078904866
Yash Chennawar <144838733+yashc73080@users.noreply.github.com>
2024-08-04 14:34:23 -0400

    still fixing things

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,27 +1,7 @@
-// // 'use client';
-
-// import { ErrorBoundary } from 'react-error-boundary';
-// import PantryClient from './PantryClient';
-
-// function ErrorFallback({error}) {
-//   return (
-//     <div role="alert">
-//       <p>Something went wrong:</p>
-//       <pre>{error.message}</pre>
-//     </div>
-//   )
-// }
-
-// export default function Home() {
-//   return (
-//     <ErrorBoundary FallbackComponent={ErrorFallback}>
-//       <PantryClient />
-//     </ErrorBoundary>
-//   );
-// }
-
 import PantryClient from './PantryClient';
 
 export default function Home() {
-  return <PantryClient />;
+  return (
+  <PantryClient />
+  );
 }
